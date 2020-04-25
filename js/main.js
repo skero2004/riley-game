8eba9f5b3303f53dbae20a3b7a75b69a16b5dd5b
Toya Takahashi <toyatakahashi522@gmail.com>
2020-04-25 09:33:22 -0800

    Add canvas

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
-// gameScene variable
+// gameScene and context variables
 const gameScene = document.getElementById("gameScene");
+const ctx = gameScene.getContext("2d");
+
+const GAME_WIDTH = gameScene.width;
+const GAME_HEIGHT = gameScene.height;
+
