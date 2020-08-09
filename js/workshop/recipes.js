59d061e79ad4d69d3fb1b7913977cfe38331bd44
Toya Takahashi <toyatakahashi522@gmail.com>
2020-08-09 15:30:28 -0800

    Updated recipe

--- js/workshop/recipes.js
+++ js/workshop/recipes.js
@@ -2,7 +2,7 @@ class Recipes {}
 
 Recipes.recipes = new Map([
 
-    ["SpaceS", "1xGears,2xWires,5xNutsAndBolts,2xMotor,1xBattery"],
-    ["OrbitalShip", "2xGears,5xWires,5xNutsAndBolts,4xMotor,2xServo,1xBattery,1xLunarCube"]
+    ["SpaceS", "1xGears,2xWires,4xNutsAndBolts,2xMotor,1xBattery"],
+    ["OrbitalShip", "2xGears,4xWires,4xNutsAndBolts,4xMotor,2xServo,1xBattery,1xLunarCube"]
 
 ]);
